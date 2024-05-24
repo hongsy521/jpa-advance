efd005c3dc3bebb725a5f0a6cdb23e32e672a60f
hongsy521 <seoyeong0521@naver.com>
2024-05-24 14:41:26 +0900

    orphanRemoval 연관엔티티 삭제

--- src/main/java/com/sparta/jpaadvance/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -18,7 +18,7 @@ public class User {
     private String name;
 
     // 양방향 지정
-    @OneToMany(mappedBy = "user",cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    @OneToMany(mappedBy = "user",cascade = CascadeType.PERSIST,orphanRemoval = true)
     private List<Food> foodList = new ArrayList<>();  // 객체를 저장하기 위한 하나의 방법일 뿐 데이터베이스 테이블에 영향을 미치지 않음
 
     public void addFoodList(Food food) {
